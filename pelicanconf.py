e64e57d79b1b4424fda2b78e2d44bacb805e2a1f
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-10-26 01:35:44 +0200

    Add pelican-quickstart files.

---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Bohumír Zámečník'
+SITENAME = 'Neural Thoughs'
+SITEURL = 'http://neural.cz'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Prague'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),)
+
+# Social widget
+SOCIAL = (('Personal website', 'http://bohumirzamecnik.cz/'),
+          ('Twitter', 'https://twitter.com/bzamecnik'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
